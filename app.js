fd72e75c3e18d8c63214458dc723e9ec59fed2e7
Jack Mardock <jdmardock@gmail.com>
2024-11-02 23:31:05 -0400

    Adding a header

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header.js';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Jack Mardock</h1>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Resume</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
